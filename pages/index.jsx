1101f1b52ff558f0c1440758d0ad3d4312bf4643
Sushant Garudkar <garudkarsushant7@gmail.com>
2022-02-28 23:40:12 +0530

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Facebook</title>
+				<meta
+					name='description'
+					content='Text your loved once and connect with people around you!'
+				/>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<h1>Hare Krishna Hare Krishna Krishna Krishna Hare Hare</h1>
+			<h1>Hare Ram Hare Ram Ram Ram Hare Hare</h1>
+		</div>
+	)
+}
